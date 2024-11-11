b2b5b8d2eaca881fe075364188d79dcfebb3203c
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-11-11 12:22:06 -0500

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = Number(e.target.elements.delay.value);
+  const step = Number(e.target.elements.step.value);
+  const amount = Number(e.target.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    const position = i;
+    const currentDelay = delay + (i - 1) * step;
+
+    createPromise(position, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
   }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
